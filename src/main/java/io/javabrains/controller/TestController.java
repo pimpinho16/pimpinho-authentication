2d55261486e02f6f594df8cf2a8bd7aab5499712
Alexander Andrade <alexander.amedina@gmail.com>
2019-11-29 07:10:09 -0600

    desarrolll básico para manejar token de sesión y autenticación

---
+++ src/main/java/io/javabrains/controller/TestController.java
@@ -0,0 +1,15 @@
+package io.javabrains.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	
+	@GetMapping(value="/test")
+	public String mojon() {
+		return "Hola";
+	}
+}
